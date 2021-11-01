2f3d29a2894a267bfacf34ac717b420ab9a1dd27
Kelsey Crow <kelseybcrow@gmail.com>
2021-11-01 16:13:29 -0600

    progress

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -260,12 +260,27 @@ emailCheck("kelsey@gmail.com");
 
 //CODE HERE
 
+function buyFrogs(gold) {
+  let amount = gold / 3;
+  return amount;
+}
+
+let totalFrogs = buyFrogs(4);
+
 ////////////////// PROBLEM 14 ////////////////////
 /*
   You might have noticed a slight bug in problem 12. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in problem 12 (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
 */
 
 //CODE HERE
+
+function buyFrogs2(gold) {
+  let amount = gold / 3;
+  let rounded = Math.floor(amount);
+  return rounded;
+}
+
+let totalFrogs2 = buyFrogs2(51);
 
 ////////////////// PROBLEM 15 ////////////////////
 let sampleArray = [0, 1, 2, 3, 4, 7, 5, 6, 8, 9];
